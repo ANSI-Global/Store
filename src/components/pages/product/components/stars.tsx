3154c96e1d792e027097824c9ec41a398a30d7ea
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-10-04 13:54:57 +0530

    Update stars.tsx

--- src/components/pages/product/components/stars.tsx
+++ src/components/pages/product/components/stars.tsx
@@ -1,24 +1,87 @@
 import { MDBIcon } from "mdbreact"
 import React from "react"
 
-const Stars = ({ rating }) => (
-  <ul className="rating">
-    <li>
-      <MDBIcon icon="star" />
-    </li>
-    <li>
-      <MDBIcon icon="star" />
-    </li>
-    <li>
-      <MDBIcon icon="star" />
-    </li>
-    <li>
-      <MDBIcon icon="star" />
-    </li>
-    <li>
-      <MDBIcon far icon="star" />
-    </li>
-  </ul>
-)
+interface props {
+  rating?: number
+}
+
+const Stars = (props: props) => {
+  const { rating = 0 } = props
+  switch (rating) {
+    case 1:
+      return (
+        <ul className="rating">
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+        </ul>
+      )
+    case 2:
+      return (
+        <ul className="rating">
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+        </ul>
+      )
+    case 3:
+      return (
+        <ul className="rating">
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+        </ul>
+      )
+    case 4:
+      return (
+        <ul className="rating">
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+        </ul>
+      )
+    case 5:
+      return (
+        <ul className="rating">
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+          <li>
+            <MDBIcon icon="star" />
+          </li>
+          <li>
+            <MDBIcon far icon="star" />
+          </li>
+        </ul>
+      )
+
+    default:
+      return null
+  }
+}
 
 export default Stars
