8404acdf5c236f1f8da75402b18c2d3e6a3ca7e6
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-10-18 18:41:40 +0530

    Update footer.tsx

--- src/components/main/components/footer.tsx
+++ src/components/main/components/footer.tsx
@@ -1,52 +1,57 @@
+import { Link } from "gatsby"
 import { MDBCol, MDBContainer, MDBFooter, MDBRow } from "mdbreact"
 import React from "react"
 
-const FooterPagePro = () => (
+const FooterPage = () => (
   <MDBFooter color="mdb-color" className="font-small pt-4 mt-4">
-    <MDBContainer className="text-center text-md-left">
+    <MDBContainer className="text-center text-md-left" fluid>
       <MDBRow className="text-center text-md-left mt-3 pb-3">
         <MDBCol md="3" lg="3" xl="3" className="mx-auto mt-3">
-          <h6 className="text-uppercase mb-4 font-weight-bold">ANSI Global</h6>
+          <h6 className="text-uppercase mb-4 font-weight-bold">
+            ANSI Global (PVT) LTD
+          </h6>
           <p>
-            Here you can use rows and columns here to organize your footer
-            content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            ANSI Global is primary Sri Lanka based company which is active in
+            eCommerce, eServices, agriculture, fishing and much more! In our
+            company customer is our primary concern.
           </p>
         </MDBCol>
         <hr className="w-100 clearfix d-md-none" />
         <MDBCol md="2" lg="2" xl="2" className="mx-auto mt-3">
-          <h6 className="text-uppercase mb-4 font-weight-bold">Products</h6>
+          <h6 className="text-uppercase mb-4 font-weight-bold">Company</h6>
           <p>
-            <a href="#!">Store</a>
+            <a href="https://store.ansiglobal.com">Store</a>
           </p>
           <p>
-            <a href="#!">Services</a>
+            <a href="https://services.ansiglobal.com">Services</a>
           </p>
           <p>
-            <a href="#!">Homepage</a>
+            <a href="https://ansiglobal.com">Homepage</a>
           </p>
           <p>
-            <a href="#!">Bootstrap Angular</a>
+            <a href="https://hire.ansiglobal.com">Jobs</a>
           </p>
         </MDBCol>
         <hr className="w-100 clearfix d-md-none" />
         <MDBCol md="3" lg="2" xl="2" className="mx-auto mt-3">
           <h6 className="text-uppercase mb-4 font-weight-bold">Useful links</h6>
           <p>
-            <a href="#!">Your Account</a>
+            <Link to="/profile">Your Account</Link>
           </p>
           <p>
-            <a href="#!">Become an Affiliate</a>
+            <Link to="/ship">Shipping Rates</Link>
           </p>
           <p>
-            <a href="#!">Shipping Rates</a>
-          </p>
-          <p>
-            <a href="#!">Help</a>
+            <Link to="/faq">Help</Link>
           </p>
         </MDBCol>
         <hr className="w-100 clearfix d-md-none" />
         <MDBCol md="4" lg="3" xl="3" className="mx-auto mt-3">
           <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
+          <p>
+            <i className="fa fa-building mr-3" />
+            Business registration: P V 00209114
+          </p>
           <p>
             <i className="fa fa-home mr-3" />
             ANSI Global (Pvt) Ltd, 115/8, Mattegoda Terrace, Mattegoda, 10240
@@ -62,41 +67,15 @@ const FooterPagePro = () => (
       </MDBRow>
       <hr />
       <MDBRow className="d-flex align-items-center">
-        <MDBCol md="8" lg="8">
-          <p className="text-center text-md-left grey-text">
+        <MDBCol>
+          <p className="text-center grey-text">
             &copy; {new Date().getFullYear()} Copyright:{" "}
             <a href="https://www.ansiglobal.com"> ansiglobal.com </a>
           </p>
-        </MDBCol>
-        <MDBCol md="4" lg="4" className="ml-lg-0">
-          <div className="text-center text-md-right">
-            <ul className="list-unstyled list-inline">
-              <li className="list-inline-item">
-                <a className="btn-floating btn-sm rgba-white-slight mx-1">
-                  <i className="fab fa-facebook-f" />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a className="btn-floating btn-sm rgba-white-slight mx-1">
-                  <i className="fab fa-twitter" />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a className="btn-floating btn-sm rgba-white-slight mx-1">
-                  <i className="fab fa-google-plus" />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a className="btn-floating btn-sm rgba-white-slight mx-1">
-                  <i className="fab fa-linkedin-in" />
-                </a>
-              </li>
-            </ul>
-          </div>
         </MDBCol>
       </MDBRow>
     </MDBContainer>
   </MDBFooter>
 )
 
-export default FooterPagePro
+export default FooterPage
