8d90fdb4824bfa2de40a44f8bddde1e923ba6d38
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-10-18 20:11:47 +0530

    Update userRating.tsx

--- src/components/pages/product/components/userRating.tsx
+++ src/components/pages/product/components/userRating.tsx
@@ -1,10 +1,11 @@
 import { useAuth } from "gatsby-theme-firebase"
+import { MDBSpinner } from "mdbreact"
 import React, { useEffect, useState } from "react"
 import UserRatingsBar from "./components/userRatingBar"
 
 interface props {
   productID: string
-  fauna:any
+  fauna: any
 }
 
 const UserRating = (props: props) => {
@@ -13,43 +14,52 @@ const UserRating = (props: props) => {
   const [rating, setRating] = useState<0 | 1 | 2 | 3 | 4 | 5>(null)
   const [isFetching, setIsFetching] = useState(true)
 
-  const { isLoading, isLoggedIn,profile } = useAuth()
+  const { isLoading, isLoggedIn, profile } = useAuth()
 
-
+  const { allProducts, allOrders } = props.fauna
 
   useEffect(() => {
     // checking if user rated and how much
-    props.fauna.allProducts.data.map(item => {
-      item.reviews.map(review=>{
-        if ( review.uid== profile.uid) {
-          setRating(review.rating)
-          setIsReviewed(true)
-        }
-        else {setIsReviewed(false)}
+    if (allProducts && allOrders) {
+      allProducts.data.map(item => {
+        item.reviews.map(review => {
+          if (review.uid == profile.uid) {
+            setRating(review.rating)
+            setIsReviewed(true)
+          } else {
+            setIsReviewed(false)
+          }
+        })
       })
- 
-    })
-    // checking if user bought the product
-    props.fauna.allOrders.data.map(order => {
-      order.products.map(product=>{
-        if ( product._id== props.productID) {
-          setIsPurchaced(true)
-        }
-        else {setIsPurchaced(false)}
+      // checking if user bought the product
+      allOrders.data.map(order => {
+        order.products.map(product => {
+          if (product._id == props.productID) {
+            setIsPurchaced(true)
+          } else {
+            setIsPurchaced(false)
+          }
+        })
       })
- 
-    })
+    }
     setIsFetching(false)
   }, [])
 
   if (!isLoading && !isFetching) {
     if (isLoggedIn) {
-      return (<UserRatingsBar uid={profile.uid} rating={rating} isReviewed={isReviewed} isPurchaced={isPurchaced}/>)
+      return (
+        <UserRatingsBar
+          uid={profile.uid}
+          rating={rating}
+          isReviewed={isReviewed}
+          isPurchaced={isPurchaced}
+        />
+      )
     } else {
       return null
     }
   } else {
-    return <p>Loading...</p>
+    return <MDBSpinner multicolor />
   }
 }
 
