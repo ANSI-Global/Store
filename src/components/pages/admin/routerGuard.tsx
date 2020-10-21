c62f02800dbf8678a3eedaeacddc5b712c522db8
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-10-21 19:15:48 +0530

    Update routerGuard.tsx

--- src/components/pages/admin/routerGuard.tsx
+++ src/components/pages/admin/routerGuard.tsx
@@ -1,15 +1,29 @@
 import { navigate } from "gatsby"
 import { useAuth } from "gatsby-theme-firebase"
+import { MDBSpinner } from "mdbreact"
 import PropTypes from "prop-types"
 
 const adminGuard = ({ children }) => {
-  const { isLoggedIn, profile } = useAuth()
-  if (isLoggedIn && profile.uid) {
-    console.log(profile.uid)
-    return children
+  const myAuth = "kea1UpSdPwS5Mvf4jn0nxnHeGEF3"
+  const { isLoading, isLoggedIn, profile } = useAuth()
+  if (!isLoading) {
+    if (isLoggedIn) {
+      if (profile.uid == myAuth) {
+        return children
+      } else {
+        return (
+          <p>
+            You are not authorized to visit admin panel, request admin for
+            clearance!
+          </p>
+        )
+      }
+    } else {
+      navigate("/user/login")
+      return null
+    }
   } else {
-    navigate("/user/login")
-    return null
+    return <MDBSpinner multicolor />
   }
 }
 
