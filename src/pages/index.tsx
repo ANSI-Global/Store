60614b21b31354670ec4f8dda757b292c2f04ebd
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-10-27 21:18:29 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,56 @@
-import React from "react"
+import { graphql } from "gatsby"
+import React, { FC } from "react"
 import Layout from "../components/main/layout"
 import SEO from "../components/main/seo"
 import Page from "../components/pages/home"
 
-const IndexPage = () => (
+interface props {
+  data: {
+    fauna: {
+      allCategories: {
+        data: {
+          _id: number
+          label: string
+        }[]
+      }
+      allProducts: {
+        data: {
+          _id: number
+          name: string
+          images: string
+          price: number
+        }[]
+      }
+    }
+  }
+}
+
+const IndexPage: FC<props> = (props: props) => (
   <Layout>
     <SEO title="Home" />
-    <Page />
+    <Page data={props.data.fauna} />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    fauna {
+      allCategories {
+        data {
+          _id
+          label
+        }
+      }
+      allProducts {
+        data {
+          _id
+          name
+          images
+          price
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
