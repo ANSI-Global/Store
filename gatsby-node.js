20a20aa803a437a50af76f948733d5720e7d19c1
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-10-27 15:50:35 +0530

    Update products

--- gatsby-node.js
+++ gatsby-node.js
@@ -20,6 +20,16 @@ exports.onCreatePage = async ({ page, actions }) => {
     createPage(page)
   }
 
+  // Only update the `/products` page.
+  if (page.path.match(/^\/products/)) {
+    // page.matchPath is a special key that's used for matching pages
+    // with corresponding routes only on the client.
+    page.matchPath = "/products/*"
+
+    // Update the page.
+    createPage(page)
+  }
+
   // Only update the `/admin` page.
   if (page.path.match(/^\/admin/)) {
     // page.matchPath is a special key that's used for matching pages
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,16 +1,43 @@
+import { Router } from "@reach/router"
 import { graphql } from "gatsby"
+import { MDBCol, MDBContainer, MDBRow } from "mdbreact"
 import React from "react"
-import ProductsPage from "../components/pages/products"
-import Carousel from "../components/pages/products/carousel"
+import Layout from "../components/main/layout"
+import List from "../components/pages/products"
+import Menu from "../components/pages/products/menu"
+import NotFound from "./404"
 
-const Products = ({ data }) => {
+interface props {
+  path
+  query?
+  data?
+}
+
+const Products = ({ data, query }: props) => {
   return (
     <>
-      <Carousel />
-      <ProductsPage data={data.fauna} />
+      <Layout>
+        <MDBContainer fluid>
+          <MDBRow>
+            <MDBCol xs="12" sm="12" md="3" lg="2" xl="2">
+              <Menu />
+            </MDBCol>
+            <MDBCol>
+              <List data={data.fauna} />
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </Layout>
     </>
   )
 }
+
+const ProductsPage = () => (
+  <Router>
+    <Products path="//products/query:query" />
+    <NotFound default />
+  </Router>
+)
 
 export const query = graphql`
   {
@@ -33,4 +60,4 @@ export const query = graphql`
   }
 `
 
-export default Products
+export default ProductsPage
