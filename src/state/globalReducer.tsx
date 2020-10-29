8e074311e6536093a1edf36d2546ff255b29386e
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-10-30 05:20:22 +0530

    Update globalReducer.tsx

--- src/state/globalReducer.tsx
+++ src/state/globalReducer.tsx
@@ -2,14 +2,17 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
 const globalSlice = createSlice({
   name: "global",
-  initialState: { currency: "Rs." },
+  initialState: { currency: "Rs.", searchBar: "" },
   reducers: {
     updateCurrency(state, action: PayloadAction<string>) {
       state.currency = action.payload
     },
+    updateSearchBar(state, action: PayloadAction<string>) {
+      state.searchBar = action.payload
+    },
   },
 })
 
-export const { updateCurrency } = globalSlice.actions
+export const { updateCurrency, updateSearchBar } = globalSlice.actions
 
 export default globalSlice.reducer
