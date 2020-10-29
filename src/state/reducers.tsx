9317f046666caaebb2c351d8ee668cb340e6e3df
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-10-30 05:20:33 +0530

    Update reducers.tsx

--- src/state/reducers.tsx
+++ src/state/reducers.tsx
@@ -2,8 +2,10 @@ import { combineReducers } from "@reduxjs/toolkit"
 import CartReducer from "../components/main/components/cartReducer"
 import AdminProductReducer from "../components/pages/admin/product/adminProductReducer"
 import ProductsReducer from "../components/pages/products/productsReducer"
+import GlobalReducer from "./globalReducer"
 
 const rootReducer = combineReducers({
+  global: GlobalReducer,
   cart: CartReducer,
   products: ProductsReducer,
   adminProduct: AdminProductReducer,
